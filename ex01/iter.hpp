0c9d83ff34b757628117fca9ab714eaeb21549cb
aroualid <aroualid@student.42.fr>
2025-09-10 17:34:06 +0200

    main and makefile to do

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -2,4 +2,12 @@
 # define ITER_HPP
 
 #include <iostream>
+
+template <typename T, typename C>
+void itter(T *array, size_t length, void(* funct)(C &arg))
+{
+	for (size_t i = 0; i < length; i++)
+		funct(array[i]);
+}
+
 #endif
